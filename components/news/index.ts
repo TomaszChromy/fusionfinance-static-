3746432b347fe993aa063b97646fbb4d96ea0bb9
Tomasz Chromy <tomasz@tomaszchromy.com>
2026-02-15 16:45:02 +0100

    Clean: Remove unused RSS components and sections - replaced by UnifiedCategoryLayout

--- components/news/index.ts
+++ components/news/index.ts
@@ -5,8 +5,7 @@
 
 export { default as NewsCard } from "../NewsCard";
 export { default as RSSArticles } from "../RSSArticles";
-export { default as RSSArticlesPaginated } from "../RSSArticlesPaginated";
-export { default as RSSArticlesInfinite } from "../RSSArticlesInfinite";
+export { default as UnifiedCategoryLayout } from "../UnifiedCategoryLayout";
 export { default as ArticleSkeleton } from "../ArticleSkeleton";
 export { default as RelatedArticles } from "../RelatedArticles";
 export { default as MarketNews } from "../MarketNews";
